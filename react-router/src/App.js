00809269b03b269609c2d597ae1141c17d6abb4f
shafaqatgithub <shafaqatmitza.jcer@gmail.com>
2025-09-18 11:33:31 +0530

    React Router Dom

---
+++ react-router/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { RouterProvider, createBrowserRouter, router } from "react-router-dom";
+
+function App() {
+  <div></div>;
+}
+
+export default App;
